3ca550cae590c12ee9f94695ae65faf51852f1f4
kotachang <i@dongju.org>
2024-10-25 10:55:58 +0900

    Restructured BLE server code, added comments, added BLE disconnect handling
    Restructured overall format and added comments for readability. BLE disconnect event now also handled to start advertising again.

--- main/soundinator_main.c
+++ main/soundinator_main.c
@@ -15,9 +15,10 @@
 #include "host/ble_hs.h"
 #include "services/gap/ble_svc_gap.h"
 #include "services/gatt/ble_svc_gatt.h"
+#include "services/ans/ble_svc_ans.h"
 #include "sdkconfig.h"
 
-char *TAG = "BLE-Server";
+char *tag = "BLE-Server";
 uint8_t ble_addr_type;
 void ble_app_advertise(void);
 
@@ -39,33 +40,37 @@ static int device_read(uint16_t con_handle, uint16_t attr_handle, struct ble_gat
 // UUID - Universal Unique Identifier
 static const struct ble_gatt_svc_def gatt_svcs[] = {
     {.type = BLE_GATT_SVC_TYPE_PRIMARY,
-     .uuid = BLE_UUID16_DECLARE(0x180),                 // Define UUID for device type
+     .uuid = BLE_UUID16_DECLARE(0xBEEF),                 // Define UUID for device type
      .characteristics = (struct ble_gatt_chr_def[]){
-         {.uuid = BLE_UUID16_DECLARE(0xFEF4),           // Define UUID for reading
+         {.uuid = BLE_UUID16_DECLARE(0xBEBE),           // Define UUID for reading
           .flags = BLE_GATT_CHR_F_READ,
           .access_cb = device_read},
-         {.uuid = BLE_UUID16_DECLARE(0xDEAD),           // Define UUID for writing
+         {.uuid = BLE_UUID16_DECLARE(0xFACE),           // Define UUID for writing
           .flags = BLE_GATT_CHR_F_WRITE,
           .access_cb = device_write},
          {0}}},
     {0}};
 
-// BLE event handling
+// BLE GAP event (connection) handling
 static int ble_gap_event(struct ble_gap_event *event, void *arg)
 {
     switch (event->type)
     {
-    // Advertise if connected
     case BLE_GAP_EVENT_CONNECT:
         ESP_LOGI("GAP", "BLE GAP EVENT CONNECT %s", event->connect.status == 0 ? "OK!" : "FAILED!");
+        /* If connect event status is not 0, then advertise again */
         if (event->connect.status != 0)
         {
             ble_app_advertise();
         }
         break;
-    // Advertise again after completion of the event
+    /* Advertisement duration over. Start advertising again */
     case BLE_GAP_EVENT_ADV_COMPLETE:
-        ESP_LOGI("GAP", "BLE GAP EVENT");
+        ESP_LOGI("GAP", "BLE GAP EVENT ADV COMPLETE");
+        ble_app_advertise();
+        break;
+    case BLE_GAP_EVENT_DISCONNECT:
+        ESP_LOGI("GAP", "BLE GAP EVENT DISCONNECT. Reason: %d", event->disconnect.reason);
         ble_app_advertise();
         break;
     default:
@@ -74,11 +79,10 @@ static int ble_gap_event(struct ble_gap_event *event, void *arg)
     return 0;
 }
 
-// Define the BLE connection
+// Define the BLE connection and start advertising
 void ble_app_advertise(void)
 {
-    printf("ble app advertise\n");
-    // GAP - device name definition
+    // GAP - set fields for device name
     struct ble_hs_adv_fields fields;
     const char *device_name;
     memset(&fields, 0, sizeof(fields));
@@ -91,34 +95,73 @@ void ble_app_advertise(void)
     // GAP - device connectivity definition
     struct ble_gap_adv_params adv_params;
     memset(&adv_params, 0, sizeof(adv_params));
-    adv_params.conn_mode = BLE_GAP_CONN_MODE_UND; // connectable or non-connectable
-    adv_params.disc_mode = BLE_GAP_DISC_MODE_GEN; // discoverable or non-discoverable
+    adv_params.conn_mode = BLE_GAP_CONN_MODE_UND; // undirected & connectable
+    adv_params.disc_mode = BLE_GAP_DISC_MODE_GEN; // general & discoverable
     ble_gap_adv_start(ble_addr_type, NULL, BLE_HS_FOREVER, &adv_params, ble_gap_event, NULL);
 }
 
-// The application
+// Start of BLE application
 void ble_app_on_sync(void)
 {
     ble_hs_id_infer_auto(0, &ble_addr_type); // Determines the best address type automatically
     ble_app_advertise();                     // Define the BLE connection
 }
 
-// The infinite task
+// The (almost) infinite FreeRTOS task
 void host_task(void *param)
 {
+    ESP_LOGI(tag, "BLE Host Task Started");
     nimble_port_run(); // This function will return only when nimble_port_stop() is executed
+
+    nimble_port_freertos_deinit();
 }
 
+/*
+* Follow programming sequence from https://docs.espressif.com/projects/esp-idf/en/v5.3.1/esp32/api-reference/bluetooth/nimble/index.html
+* as well as the bleprph example by espressif
+*/
 void app_main()
 {
-    nvs_flash_init();                          // 1 - Initialize NVS flash using
-    nimble_port_init();                        // 3 - Initialize the host stack
-    ble_svc_gap_device_name_set("BLE-Server-Test"); // 4 - Initialize NimBLE configuration - server name
-    ble_svc_gap_init();                        // 4 - Initialize NimBLE configuration - gap service
-    ble_svc_gatt_init();                       // 4 - Initialize NimBLE configuration - gatt service
-    ble_gatts_count_cfg(gatt_svcs);            // 4 - Initialize NimBLE configuration - config gatt services
-    ble_gatts_add_svcs(gatt_svcs);             // 4 - Initialize NimBLE configuration - queues gatt services.
-    ble_hs_cfg.sync_cb = ble_app_on_sync;      // 5 - Initialize advertisement only after the host & controller are synced
-    ble_hs_cfg.reset_cb = ble_app_on_sync;     // todo cleanup comments - start advertisement process again if NimBLE stack crashes and resets
-    nimble_port_freertos_init(host_task);      // 6 - Run the thread
+    /* Initialize Non Volatile Storage — it is used to store physical layer calibration data */
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    /* Initialize the NimBLE protocol stack */
+    ret = nimble_port_init();
+    if (ret != ESP_OK) {
+        ESP_LOGE(tag, "Failed to init nimble %d ", ret);
+        return;
+    }
+
+    /* Initialize the NimBLE host configuration. */
+    ble_hs_cfg.sync_cb = ble_app_on_sync;   // Initialize advertisement only after the host & controller are synced
+    ble_hs_cfg.reset_cb = ble_app_on_sync;  // If NimBLE stack resets after crash, start advertisement process again
+    /* TODO: if implemeting persistent connection then add
+    * ble_hs_cfg.store_status_cb = ble_store_util_status_rr;
+    * and look into 'store' 
+    */
+    /* TODO: if implementing Security Manager features, set the host settings here
+    * ex) secure connection: ble_hs_cfg.sm_sc = 1;
+    * Note: if remote device doesn't have secure connection abilities connection fails
+    */
+
+    /* Initialize GAP, GATT, ANS services */
+    int rc;
+    ble_svc_gap_init();     // General Access Profile
+    ble_svc_gatt_init();    // General Attribute Profile
+    ble_svc_ans_init();     // Alert and Notifications
+    rc = ble_gatts_count_cfg(gatt_svcs);
+    assert(rc == 0);
+    rc = ble_gatts_add_svcs(gatt_svcs);
+    assert(rc == 0); 
+
+    /* Name BLE Device */
+    ble_svc_gap_device_name_set("Soundinator");
+    
+    /* Run BLE in a new FreeRTOS task */
+    nimble_port_freertos_init(host_task);
 }
