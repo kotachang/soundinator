6187d6f685c12f35ec90296fcf734a4a16d8226b
kotachang <i@dongju.org>
2024-11-09 11:32:19 +0900

    Work in progress - playing a sample wave
    Started implementing the main player task to write audio with i2s_write. Work in progress - still builds but should NOT be flashed

--- ESP32/main/inc/player.hpp
+++ ESP32/main/inc/player.hpp
@@ -10,29 +10,29 @@
 #include "freertos/queue.h"
 #include "esp_mac.h"
 
-class Player {
-    public:
-        Player();
-        ~Player();
+class Player
+{
+public:
+    Player();
+    ~Player();
 
-        void init();
+    void init();
+    int play();
 
-    private:
-        i2s_port_t i2s_port;
-        static const i2s_driver_config_t i2s_config;
-        QueueHandle_t i2s_queue;
-        
-        /* I2S pins */
-        i2s_pin_config_t i2s_pins = {
-            .bck_io_num = 26,
-            .ws_io_num = 25,
-            .data_out_num = 22,
-            .data_in_num = -1
-        };
+private:
+    i2s_port_t i2s_port = I2S_NUM_0;
+    static const i2s_driver_config_t i2s_config;
+    static QueueHandle_t i2s_queue;
 
-        static TaskHandle_t player_task_handle;
-        static void _i2s_player(void *param);
+    /* I2S pins */
+    i2s_pin_config_t i2s_pins = {
+        .bck_io_num = 26, // TODO: define constants
+        .ws_io_num = 25,
+        .data_out_num = 22,
+        .data_in_num = -1};
 
+    static TaskHandle_t player_task_handle;
+    static void _player_task(void *param);
 };
 
 #endif /* PLAYER_HPP */
--- ESP32/main/src/player.cpp
+++ ESP32/main/src/player.cpp
@@ -7,7 +7,7 @@ Private Member Initialization
 */
 const i2s_driver_config_t Player::i2s_config = {
     .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
-    .sample_rate = 44100, // TODO: set this based on BLE input
+    .sample_rate = 44100, // TODO: set this based on BLE input. might have to use i2s_set_sample_rates(i2s_port, rate)
     .bits_per_sample = (i2s_bits_per_sample_t)16, // TODO: set this based on BLE input
     .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
     .communication_format = I2S_COMM_FORMAT_STAND_I2S,
@@ -41,10 +41,18 @@ void Player::init() {
     i2s_set_pin(i2s_port, &i2s_pins);
     // clear the DMA buffers
     i2s_zero_dma_buffer(i2s_port);
-    i2s_set_sample_rates(i2s_port, 22050); //set sample rates
 
-    xTaskCreate(_i2s_player, "I2S Player Task", 4096, this, 1, &player_task_handle);
+    /* Start the main player task */
+    // TODO: check stack size
+    // TODO: define priority macros
+    xTaskCreate(_player_task, "I2S Player Task", 4096, nullptr, 1, &player_task_handle);
     //TODO: i2s_driver_uninstall(i2s_num); //stop & destroy i2s driver
+}
+
+/* Play audio TODO: implement communication with BLE task */
+int Player::play() {
+    // TODO: check if player task already exists
+    return 0;
 }
 
 
@@ -55,9 +63,17 @@ Private Functions
 */
 
 /* Main player task */
-void Player::_i2s_player(void *param) {
+void Player::_player_task(void *param) {
     size_t len = 100; // in bytes
     uint8_t bytes[len] = {0};
     size_t i2s_bytes_written;
-    i2s_write(I2S_NUM_0, &bytes, len, &i2s_bytes_written, portMAX_DELAY);
+    // TODO: might be expecting unsigned 16 bit info if it seems not to work
+
+    i2s_event_t event;
+    while (true) {
+        if (xQueueReceive(i2s_queue, &event, portMAX_DELAY) == pdPASS) {
+            i2s_write(I2S_NUM_0, &bytes, len, &i2s_bytes_written, portMAX_DELAY);
+        }
+    }
 }
+
